f7fdecd656ff752164b1d2c88be335dc1976b9db
dawmro <dawidmrotek@protonmail.ch>
2023-07-18 15:42:57 -0700

    [ADD] file uploads

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 import pathlib
 import os
+import io
+import uuid
 from functools import lru_cache
-from fastapi import FastAPI, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException, Depends, Request, File, UploadFile
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     debug: bool = False
+    echo_active: bool = False
 
     class Config:
         env_file = ".env"
@@ -20,18 +23,33 @@ def get_settings():
 settings = get_settings()
 DEBUG = settings.debug
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploaded"
 
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
+
 @app.get("/", response_class=HTMLResponse)
 def home_view(request: Request, settings:Settings = Depends(get_settings)):
-    print(settings.debug)
     context = {
         "request": request
     }
     return templates.TemplateResponse("home.html", context=context)
 
+
 @app.post("/")
 def home_detail_view():
     return {"hello": "world"}
+
+
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo_view(file:UploadFile = File(...), settings:Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(detail="Invalid Endpoint", status_code=400)
+    bytes_str = io.BytesIO(await file.read())
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
+    with open(str(dest), 'wb') as out:
+        out.write(bytes_str.read())
+    return dest
